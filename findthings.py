b90857b277e28c9b37f41cbaf48df5e6c8943b0e
Lukas Schaub <lukas_schaub@gmx.ch>
2020-11-08 21:20:55 +0100

    marked down the idea

---
+++ findthings.py
@@ -0,0 +1,16 @@
+
+#1 find things useing google schoolar
+
+#2 read pdfs based on dois useing sci-hub
+
+#3 get more important papers based on inital results
+
+#4 save all dois in a txt doc
+
+#5 for unique dois check for key words based on search and and mark the papers
+
+#6 retrun a list of the most important papers print them if needed
+
+
+
+
